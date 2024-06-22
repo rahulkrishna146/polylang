56bc03baaf77cc15e35aaa7b75c6292ab9b31320
Rahulkrish <rahulkrishna146@gmail.com>
2024-06-22 12:06:11 +0530

    cosine lr implemented

--- train_polylang.py
+++ train_polylang.py
@@ -238,8 +238,27 @@ model = torch.compile(model)
 #print(logits.shape)
 #print(loss.item())
 
+max_lr = 6e-4
+min_lr = max_lr * 0.1
+warmup_steps = 10
+max_steps = 50
+
+def get_lr(it):
+    # 1) linear warmup for warmup_iters steps
+    if it < warmup_steps:
+        return max_lr * (it+1) / warmup_steps
+    # 2) if it > lr_decay_iters, return min learning rate
+    if it > max_steps:
+        return min_lr
+    # 3) in between, use cosine decay down to min learning rate
+    decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff starts at 1 and goes to 0
+    return min_lr + coeff * (max_lr - min_lr)
+
+#opimizer
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4,betas=(0.9, 0.95), eps = 1e-8)
-for i in range(50):
+for step in range(max_steps):
     t0 = time.time()
     batch = next(iter(train_loader))
     x = batch['bert_input'].to(device)
@@ -250,12 +269,16 @@ for i in range(50):
         logits, loss = model(x, y)
     loss.backward()
     norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+    # determine and set the learing rate for this iteration
+    lr = get_lr(step)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
     optimizer.step()
     torch.cuda.synchronize()
     t1 = time.time()
     dt = (t1-t0)*1000 # time diff in milliseconds
     tokens_per_sec = (batch_size*block_size)/(t1-t0)
-    print(f"step{i:4d} | loss: {loss.item():6f} | norm: {norm:4f} | dt: {dt:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
+    print(f"step{step:4d} | loss: {loss.item():6f} | lr:{lr:4e} | norm: {norm:4f} | dt: {dt:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
 
 import sys; sys.exit(0)
 #import code; code.interact(local=locals())
